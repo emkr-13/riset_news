6f57ca120e2261b8ae57eed938bafdcc69cd66cf
emkr-13 <krisna.rata13@gmail.com>
2024-02-27 09:09:23 +0700

    update aja

--- duckgo.py
+++ duckgo.py
@@ -1,5 +1,11 @@
+import json
 from duckduckgo_search import DDGS
 
 with DDGS() as ddgs:
     results = [r for r in ddgs.text("jokowi :: kompas", max_results=100)]
-    print(results)
+
+# Menyimpan hasil pencarian ke dalam file JSON
+with open('search_results.json', 'w') as json_file:
+    json.dump(results, json_file, indent=4)
+
+print("Hasil pencarian telah disimpan dalam file 'search_results.json'.")
